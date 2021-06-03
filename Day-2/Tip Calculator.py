dee3dd2a878ffb3c1662e7cd8bc259d3ae7c4921
MohanVamshi29 <85226227+MohanVamshi29@users.noreply.github.com>
2021-06-03 18:24:25 +0530

    Add files via upload

---
+++ Day-2/Tip Calculator.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+age = int(input("What is your current age?\n"))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age_remained=90-age
+days_left=age_remained*365
+weeks_left=age_remained*52
+months_left=age_remained*12
+
+result=print(f"You have {days_left} days,{weeks_left} weeks, and {months_left} months left.")
+
+
+
+
+
+
+
